9fe87c18bede490eda46b03dc449c04666b71fc3
Soumo Sarkar <soumosarkar.official@gmail.com>
2025-07-06 21:04:40 +0530

    Refactor installation scripts to remove unnecessary flags and improve command clarity

--- cluster-setup/scripts/install_master.sh
+++ cluster-setup/scripts/install_master.sh
@@ -91,8 +91,8 @@ apt-get install -y apt-transport-https ca-certificates curl gpg
 mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get --allow-unauthenticated update
-apt-get --allow-unauthenticated install -y containerd kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION} kubernetes-cni
+apt-get update
+apt-get install -y containerd kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION} kubernetes-cni
 
 
 
