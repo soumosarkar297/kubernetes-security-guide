c55a3f3a6d5e1e9b8a39f59c56e73745e33406b8
Soumo Sarkar <soumosarkar.official@gmail.com>
2025-07-06 20:52:32 +0530

    Fix Kubernetes apt keyring path in installation scripts

--- cluster-setup/scripts/install_master.sh
+++ cluster-setup/scripts/install_master.sh
@@ -90,28 +90,11 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates
 mkdir -p /etc/apt/keyrings
 rm /etc/apt/keyrings/kubernetes-1-33-apt-keyring.gpg || true
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-1-32-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-1-33-apt-keyring.gpg
 echo > /etc/apt/sources.list.d/kubernetes.list
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-1-33-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get --allow-unauthenticated update
 apt-get --allow-unauthenticated install -y containerd kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION} kubernetes-cni
-
-
-
-### configure containerd
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
-sudo modprobe overlay
-sudo modprobe br_netfilter
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
-sudo sysctl --system
-sudo mkdir -p /etc/containerd
 
 
 
@@ -129,6 +112,7 @@ net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 sudo sysctl --system
 sudo mkdir -p /etc/containerd
+
 
 
 ### containerd config
